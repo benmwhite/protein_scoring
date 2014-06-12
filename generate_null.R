9f055cb1bf40553b2ae91f856817745fa08873a9
benmwhite <bw2121@gmail.com>
2014-06-12 02:51:08 -0500

    simulating null experiments

---
+++ generate_null.R
@@ -0,0 +1,49 @@
+#function to generate a reference null proteome given a set of 
+#purification experiments
+source("init_fns.R")
+
+#the *_col arguments should be strings refering to the appropriate
+#variable names in the data frame. nprey is the cap on the number of unique
+#preys to appear in each run, TSC is the cap on the total counts 
+#to appear in each run. By default nprey is the average number of 
+#unique preys found in the original experiments rounded up to the n
+#nearest integer. Similarly the default TSC is the average TSC accross
+#all the original experiments rounded up to the nearest integer
+
+generate_null <- function(data, bait_col, prey_col, count_col,
+                          nprey = NULL, TSC = NULL) {
+  data <- data %.%
+    rename_col(bait_col, "Bait") %.%
+    rename_col(prey_col, "Prey") %.%
+    rename_col(count_col, "Count")
+  bait_info <- group_by(data, Bait) %.%
+    summarise(unique_preys = length(Prey), TSC = sum(Count))
+  if (is.null(nprey)) {
+    nprey <- ceiling(median(bait_info$unique_preys))   
+  }
+  if (is.null(TSC)) {
+    TSC <- ceiling(median(bait_info$TSC))
+  }
+  #setting up vector of preys and weights
+  old_preycounts <- group_by(data, Prey) %.%
+    summarise(TSC = sum(Count))
+  #picking preys to show up in experiment
+  new_preys <- vector("character")
+  i <- 1
+  while (length(unique(new_preys)) < nprey) {
+    #draw
+    new_preys[[i]] <- sample(old_preycounts$Prey, size = 1, 
+                             prob = old_preycounts$TSC)
+    i <- i + 1
+  }
+  #filling out prey counts until desired TSC is reached
+  subset_preycounts <- filter(old_preycounts, Prey %in% new_preys)
+  for (i in (length(new_preys) + 1):TSC) {
+    new_preys[[i]] <- sample(subset_preycounts$Prey, size = 1,
+                             prob = subset_preycounts$TSC)
+  }
+  out <- tbl_df(data.frame(table(new_preys))) %.%
+                  rename_col("new_preys", "Prey") %.%
+                  rename_col("Freq", "Count")
+  return(out)
+}
--- scratch.R
+++ scratch.R
@@ -1,27 +1,59 @@
 #scratch work for scoring
-#setwd("~/Dropbox/School/Research/protein_scoring")
+setwd("~/Dropbox/School/Research/protein_scoring")
 source("init_fns.R")
 source("mink_dist.R")
+library("stringr")
 
 #loading data, removing 1 and 2 counts, normalizing by length
-dat <- tbl_df(read.csv("~/Dropbox/School/Research/Minkowski/Chen-all.csv",
-              header = TRUE, stringsAsFactors = FALSE)) %.%
-  filter(Count > 2) %.%
-  apply_col("Length", function(x) {x[is.na(x)] <- 300; return(x)}) %.%
-  mutate(normCount = Count/Length)
+dat <- tbl_df(read.csv("~/Dropbox/School/Research/DUB.csv",
+              header = FALSE, stringsAsFactors = FALSE))
+names(dat) <- c("Bait", "Prey", "Count")
+dat <- group_by(dat, "Bait", "Prey") %.%
+  summarise(Count = sum(Count)) %.%
+  filter(Bait != Prey, Count > 1) %.%
+  ungroup()
 
-dists <- compute_dist(dat, "Prey", "normCount", 0.25)
-qplot(log(D_i), data = dists)
+data2 <- tbl_df(read.csv("~/Dropbox/School/Research/Minkowski/Chen-new.csv", 
+                         header = TRUE,
+                         stringsAsFactors = FALSE)) %.%
+  rename_col("reference", "Prey") %.%
+  group_by(ExpNo, Prey) %.% 
+  summarise(Count = sum(Total)) %.%
+  apply_col("Prey", str_trim) %.%
+  filter(Count > 2) %.%
+  ungroup() 
+
+dat <- apply_col(dat, "Count", function(x) {x[x>50] <- 50; x})
+n_exp <- length(unique(dat$Bait))
+dists <- compute_dist(dat, "Prey", "Count", 0.3) %.%
+  group_by(Prey) %.%
+  mutate(Specificity = length(Prey) / n_exp) %.%
+  ungroup()
+qplot(D_i, data = dists)
+#poisson scoring
+scores <- dists %.%
+  select(-D_ik) %.%
+  mutate(Pois_score = ppois(Count, D_i * Specificity / n_exp))
+arrange(scores, Prey)[1:40,]
+write.csv()
+qplot(Pois_score, data = scores)
+
+ps <- seq(from = 0.1, to = 0.6, by = 0.02)
+diffs <- diffs_pseq(dat, ps, "Prey", "Count", M_diff)
+diffs2 <- diffs_pseq(data2, ps, "Prey", "Count", MCV)
+qplot(p, MeanDiff, data = diffs, geom = c("point", "line"),
+      main = "Average M_i's (DUB data set)",
+      xlab = "p (Minkowski power parameter)",
+      ylab = "Average M_i")
 
 #looking at prey specificity
 spec <- group_by(dat, Prey) %.%
   summarise(occurrences = length(Prey), 
             Length = max(Length),
-            medTSC = median(Count),
-            meanTSC = mean(Count)) %.%
+            TSC = sum(Count)) %.%
   ungroup()
 qplot(Length, occurrences, data = spec, geom = "bin2d")
-qplot(log(occurrences), data = spec)
+qplot(occurrences, data = spec, binwidth = 1, xlim = c(0, 20))
 qplot(log(medTSC), log(meanTSC), data = spec)
 
 #looking at total spectral counts accross baits
@@ -29,3 +61,26 @@ bait_sc <- group_by(dat, ExpNo) %.%
   summarise(TSC = sum(Count), meanTSC = mean(Count)) %.%
   ungroup()
 qplot(TSC, meanTSC, data = bait_sc, main = "Bait TSC")
+
+#testing Zs
+Zs <- function(dists) {
+  out <- dists %.% 
+    group_by(Prey) %.%
+    mutate(Z = (D_ik - mean(D_ik)) / sd(D_ik)) %.%
+    ungroup()
+}
+test <- Zs(compute_dist(dat, "Prey", "Count", 0.3))
+
+#adding CRAPome information
+
+crap <- tbl_df(read.csv("~/Dropbox/School/Research/HIPPO/CRAPome.csv", 
+                        header = TRUE, stringsAsFactors = FALSE)) %.%
+  rename_col("GENE", "Prey")
+
+scores_crap <- left_join(scores, crap) %.%
+  select(-(Num.Expt:Ave.SC))
+scores_crap <- scores_crap %.%
+  apply_col("Pois_score", round, 4) %.%
+  apply_col("Specificity", round, 4) %.%
+  apply_col("D_i", round, 4)
+write.csv(scores_crap, "scores_june10.csv", row.names = FALSE)
