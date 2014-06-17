79035a848f0704e1ed2d039b1416921097f46232
benmwhite <bw2121@gmail.com>
2014-06-17 12:17:24 -0500

    added function for checking overlap with CRAPOME

---
+++ CRAPOME.R
@@ -0,0 +1,18 @@
+#functions for comparing scores to CRAPOME
+source("init_fns.R")
+library("dplyr")
+
+crap <- tbl_df(read.csv("~/Dropbox/School/Research/HIPPO/CRAPome.csv", 
+                        header = TRUE, stringsAsFactors = FALSE)) %.%
+  rename_col("GENE", "Prey") %.%
+  select(-(Num.Expt:Ave.SC))
+
+crap_fraction <- function(data, prey_col, score_col, 
+                       score_thresh, crap_thresh) {
+  final_scores <- left_join(data, crap) %.%
+    rename_col(score_col, "Score") %.%
+    rename_col(prey_col, "Prey") %.%
+    filter(Score > score_thresh)
+  ncrap <- nrow(filter(final_scores, Frequency > crap_thresh))
+  return(ncrap / nrow(final_scores))
+}
